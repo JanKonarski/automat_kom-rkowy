28bd360a727994b5d0b27fff47e477ed2ba0746e
JanKonarski <61757930+JanKonarski@users.noreply.github.com>
2020-03-07 14:08:46 +0100

    Add files via upload

--- engine.c
+++ engine.c
@@ -6,7 +6,12 @@
 
 #include "engine.h"
 
-static size_t add_y_list (truss_t** tr, size_t x, size_t y) {
+static size_t add_y_list (truss_t** tr, size_t x, size_t y)
+{
+	if ( x > (*tr)->width ||
+		 y > (*tr)->height )
+		
+		return false;
 	
 	if ( (*tr) == NULL )
 		return false;
@@ -19,7 +24,7 @@ static size_t add_y_list (truss_t** tr, size_t x, size_t y) {
 			
 		(*tr)->cols = (*tr)->cols->next;
 	
-		y_list_t* node = (y_list_t*)malloc( sizeof( y_list_t ) );
+		y_list_t* node = (y_list_t*)malloc( sizeof( y_list_t ));
 		
 		node->back = NULL;
 		node->next = NULL;
@@ -29,8 +34,8 @@ static size_t add_y_list (truss_t** tr, size_t x, size_t y) {
 		if ( (*tr)->cols->rows == NULL )
 			(*tr)->cols->rows = node;
 			
-		else {
-	
+		else
+		{
 			while ( (*tr)->cols->rows->next &&
 					(*tr)->cols->rows->y < y ) {
 				
@@ -41,19 +46,19 @@ static size_t add_y_list (truss_t** tr, size_t x, size_t y) {
 				return false;
 			
 			if ( (*tr)->cols->rows->y < y &&
-				 (*tr)->cols->rows->next == NULL ) {
-				
+				 (*tr)->cols->rows->next == NULL )
+			{
 				node->back = (*tr)->cols->rows;
 				(*tr)->cols->rows->next = node;
 				 	
 			} else if ( (*tr)->cols->rows->y > y &&
-						(*tr)->cols->rows->back == NULL ) {
-				
+						(*tr)->cols->rows->back == NULL )
+			{
 				node->next = (*tr)->cols->rows;
 				(*tr)->cols->rows->back = node;
 					
-			} else {
-				
+			} else
+			{	
 				node->back = (*tr)->cols->rows->back;
 				node->next = (*tr)->cols->rows;
 				
@@ -76,10 +81,12 @@ static size_t add_y_list (truss_t** tr, size_t x, size_t y) {
 	return true;
 }
 
-static void add_x_list (truss_t** tr) {
+static void add_x_list (truss_t** tr)
+{
 	size_t i;
-	for ( i = 0; i < (*tr)->width; ++i ) {
-		x_list_t* node = (x_list_t*)malloc( sizeof(x_list_t) );
+	for ( i = 0; i < (*tr)->width; ++i )
+	{
+		x_list_t* node = (x_list_t*)malloc( sizeof( x_list_t ));
 		
 		node->back = (*tr)->cols;
 		node->next = NULL;
@@ -87,7 +94,8 @@ static void add_x_list (truss_t** tr) {
 		node->y_count = 0;
 		node->rows = NULL;
 		
-		if ( (*tr)->cols ) {
+		if ( (*tr)->cols )
+		{
 			while ( (*tr)->cols->next )
 				(*tr)->cols = (*tr)->cols->next;
 			
@@ -101,11 +109,12 @@ static void add_x_list (truss_t** tr) {
 		(*tr)->cols = (*tr)->cols->back;
 }
 
-truss_t* new_truss (size_t width, size_t height) {
+truss_t* new_truss (size_t width, size_t height)
+{
 	if ( width == 0 || height == 0 )
 		return NULL;
 	
-	truss_t* tr = (truss_t*)malloc( sizeof(truss_t) );
+	truss_t* tr = (truss_t*)malloc( sizeof (truss_t ));
 	tr->width = width;
 	tr->height = height;
 	tr->e_count = 0;
@@ -120,21 +129,45 @@ truss_t* load_truss (char* file_name) {
 	
 }
 
-void rand_truss (truss_t** tr, size_t count) {
+void rand_truss (truss_t** tr, size_t count)
+{
+	size_t width = (*tr)->width;
+	size_t height = (*tr)->height;
+	if ( count > width * height )
+		return;
 	
+	size_t i = 0;
+	while ( i < count )
+		if ( !add_y_list( tr,
+						  rand() % (*tr)->width + 1,
+						  rand() % (*tr)->height + 1 ))
+			++i;
 }
 
 
 
-static void del_y_list (y_list_t** yli, size_t x, size_t y) {
+static void del_y_list (y_list_t** yli, size_t x, size_t y)
+{
 	
 }
 
-static void del_x_list (x_list_t** xli, size_t x) {
+static void del_x_list (x_list_t** xli, size_t x)
+{
 	
 }
 
-void del_truss (truss_t** tr) {
+void del_truss (truss_t** tr)
+{
+	
+}
+
+void next_generation (truss_t** tr)
+{
+	
+}
+
+void show_truss (truss_t *tr)
+{
 	
 }
 
